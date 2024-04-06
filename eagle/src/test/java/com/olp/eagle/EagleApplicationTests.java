639510dae2865517e81d20b99304dc25a30b401d
DuleepAlwis <duleepalwis@gmail.com>
2024-04-06 17:57:28 +0530

    Changes with unit testing

--- eagle/src/test/java/com/olp/eagle/EagleApplicationTests.java
+++ eagle/src/test/java/com/olp/eagle/EagleApplicationTests.java
@@ -1,10 +1,15 @@
 package com.olp.eagle;
 
+import com.olp.controller.UserController;
+import com.olp.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EagleApplicationTests {
+
 
 	@Test
 	void contextLoads() {
